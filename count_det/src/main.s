0dc4609fbd88f98c57d7a6e9af59e8cc4e9c43b8
Misha Bernovskiy <mikhail.bernovskiy@gmail.com>
2020-09-29 20:01:43 +0300

    3x3 matrix mult in ARM ASM

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,35 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+    ldr r1, [r0], #4
+    ldr r2, [r0], #4
+    ldr r3, [r0], #4
+    ldr r4, [r0], #4
+    ldr r5, [r0], #4
+    ldr r6, [r0], #4
+    ldr r7, [r0], #4
+    ldr r8, [r0], #4
+    ldr r9, [r0], #4
+
+    mov r0, #0
+
+    mul r0, r5, r9
+    mul r10, r6, r8
+    sub r0, r10
+    mul r1, r0
+
+    mul r0, r6, r7
+    mul r10, r4, r9
+    sub r0, r10
+    mul r2, r0
+
+    mul r0, r4, r8
+    mul r10, r5, r7
+    sub r0, r10
+    mul r3, r0
+
+    add r0, r1, r2
+    add r0, r3
+
+    bx lr
